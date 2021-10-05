314c3d0bd5ddec40262630d1c3b57e999a67360d
san <sanjevareddy.ch@gmail.com>
2021-10-05 10:39:43 +0530

    w#7

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -1,56 +1,59 @@
 @Library('shared-library') _
 
 pipeline {
-    agent any
+  agent any
 
-      parameters {
-                string defaultValue: 'master', name: 'Branch', trim: true
-                }
-
-      environment {
-                Build_by = "Bazel"
-      }
-
-    stages {
-        stage('Build') {
-            steps {
-		
-                echo 'git checkout from '+ params.Branch
-                git branch: params.Branch, credentialsId: 'git-creds', url: 'https://github.com/bazelbuild/examples'
-		sh 'ls -ltr'
-		sh 'pwd'
-                script {
-                    if (env.Build_by == 'Bazel'){
-			sh 'pwd'
-                        enironment('bazel')
-                        echo "Building project using Bazel "
-			sh 'pwd'
-			
-                        sh '''
-			    ls -ltr cpp-tutorial
-                            cd cpp-tutorial/stage1
-                            bazel build //main:hello-world
-                            '''
-                    } else {
-                    echo "This build system is not configured"
-                    }
-                }
-            }
-        }
-        stage('test') {
-            steps {
-                sh '''
-                cd cpp-tutorial/stage1/bazel-bin/main
-                ./hello-world
-                '''
-            }
-        }
-	post {
-  		always {
-   			deleteDir()
+  parameters {
+    string defaultValue: 'master', name: 'Branch', trim: true
   }
-}
 
+  environment {
+    Build_by = "Bazel"
+  }
+
+  stages {
+    stage('Build') {
+      steps {
+
+        echo 'git checkout from ' + params.Branch
+        git branch: params.Branch, credentialsId: 'git-creds', url: 'https://github.com/bazelbuild/examples'
+        sh 'ls -ltr'
+        sh 'pwd'
+        script {
+          if (env.Build_by == 'Bazel') {
+            sh 'pwd'
+            enironment('bazel')
+            echo "Building project using Bazel "
+            sh 'pwd'
+
+            sh ''
+            '
+            ls - ltr cpp - tutorial
+            cd cpp - tutorial / stage1
+            bazel build //main:hello-world
+              ''
+            '
+          } else {
+            echo "This build system is not configured"
+          }
+        }
+      }
     }
-}
+    stage('test') {
+      steps {
+        sh ''
+        '
+        cd cpp - tutorial / stage1 / bazel - bin / main
+          . / hello - world ''
+        '
+      }
+    }
 
+  }
+  post {
+    always {
+      deleteDir()
+    }
+  }
+
+}
