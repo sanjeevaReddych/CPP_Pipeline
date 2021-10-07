d6057e213f7c277a13d3184b91564d3f85b3de1a
san <sanjevareddy.ch@gmail.com>
2021-10-08 04:11:59 +0530

    w#1

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -23,7 +23,7 @@ pipeline {
             bazel_helper.prepare_environment()
 
             echo "Building project using Bazel "
-            bazel_helper.create_build('cpp-tutorial/stage1', 'hello-world')
+            bazel_helper.create_build(dir: 'cpp-tutorial/stage1', build_function: 'hello-world')
 
            
           } else {
